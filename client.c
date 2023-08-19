7015c3bd030a851fc92b0b80463cde4428c732f8
saho-north <SahoKitahara@my.uopeople.edu>
2023-08-19 23:56:28 +0900

    working on ACK

--- client.c
+++ client.c
@@ -6,7 +6,7 @@
 /*   By: sakitaha <sakitaha@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/15 04:31:32 by sakitaha          #+#    #+#             */
-/*   Updated: 2023/08/19 03:57:52 by sakitaha         ###   ########.fr       */
+/*   Updated: 2023/08/19 05:28:54 by sakitaha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,6 +24,40 @@
  */
 
 static volatile sig_atomic_t	g_signal_status;
+
+static void	exit_with_error(t_error_type error_type)
+{
+	const char	*message[10];
+
+	message[TOO_MANY_ARGS] = "Error: Too many arguments";
+	message[TOO_FEW_ARGS] = "Error: Too few arguments";
+	message[NOT_VALID_PID] = "Error: Invalid PID";
+	message[NOT_VALID_STRING] = "Error: Invalid string";
+	message[NOT_VALID_CHAR] = "Error: Invalid char";
+	message[KILL_FAILED] = "Error: Failed to send signal";
+	ft_putendl_fd(message[error_type], 2);
+	exit(1);
+}
+
+static void	exit_with_error(t_error_type error_type)
+{
+	const char *message[ERROR_TYPE_COUNT]; // サイズを ERROR_TYPE_COUNT に変更
+	message[TOO_MANY_ARGS] = "Error: Too many arguments";
+	message[TOO_FEW_ARGS] = "Error: Too few arguments";
+	message[NOT_VALID_PID] = "Error: Invalid PID";
+	message[NOT_VALID_STRING] = "Error: Invalid string";
+	message[NOT_VALID_CHAR] = "Error: Invalid char";
+	message[KILL_FAILED] = "Error: Failed to send signal";
+	if (error_type < ERROR_TYPE_COUNT)
+	{ // 追加: エラーの範囲のチェック
+		ft_putendl_fd(message[error_type], 2);
+	}
+	else
+	{
+		ft_putendl_fd("Error: Unknown error", 2); // 追加: 未定義のエラーケース
+	}
+	exit(1);
+}
 
 static void	signal_handler(int sig)
 {
@@ -60,8 +94,10 @@ static t_signal_acknowledgement	is_responce_valid(void)
 static void	transmit_char(pid_t pid, char c)
 {
 	size_t	bit_index;
+	size_t	counter_for_retry;
 
 	bit_index = 8;
+	counter_for_retry = 0;
 	while (bit_index-- > 0)
 	{
 		g_signal_status = -1;
@@ -83,12 +119,16 @@ static void	transmit_char(pid_t pid, char c)
 				exit(1);
 			}
 		}
-		// if (is_responce_valid() == SERVER_BUSY)
-		// {
-		// 	usleep(SLEEP_DURATION);
-		// 	continue ;
-		// }
-		usleep(SLEEP_DURATION);
+		if (is_responce_valid() != SIGNAL_RECEIVED)
+		{
+			counter_for_retry++;
+			if (counter_for_retry > 10)
+			{
+				ft_putendl_fd("Error: Failed to send signal", 2);
+				exit(1);
+			}
+			continue ;
+		}
 	}
 	//ft_putchar_fd('\n', 1);
 }
--- minitalk.h
+++ minitalk.h
@@ -6,7 +6,7 @@
 /*   By: sakitaha <sakitaha@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/15 04:12:35 by sakitaha          #+#    #+#             */
-/*   Updated: 2023/08/19 03:51:43 by sakitaha         ###   ########.fr       */
+/*   Updated: 2023/08/19 05:28:50 by sakitaha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,6 +28,17 @@ typedef enum e_signal_acknowledgement
 	SIGNAL_RECEIVED,
 	WAITING_FOR_ACK
 }		t_signal_acknowledgement;
+
+typedef enum e_error_type
+{
+	TOO_MANY_ARGS,
+	TOO_FEW_ARGS,
+	NOT_VALID_PID,
+	NOT_VALID_STRING,
+	NOT_VALID_CHAR,
+	KILL_FAILED,
+	ERROR_TYPE_COUNT
+}		t_error_type;
 
 bool	is_valid_pid(const char *str);
 //デバッグ用
