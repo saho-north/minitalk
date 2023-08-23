72d8f4ab398526a101f526014dc75de8b4956638
saho-north <SahoKitahara@my.uopeople.edu>
2023-08-24 05:42:37 +0900

    ok

--- server.c
+++ server.c
@@ -6,7 +6,7 @@
 /*   By: sakitaha <sakitaha@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/15 03:56:21 by sakitaha          #+#    #+#             */
-/*   Updated: 2023/08/24 05:22:52 by sakitaha         ###   ########.fr       */
+/*   Updated: 2023/08/24 05:36:26 by sakitaha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -102,7 +102,7 @@ int	main(void)
 }
  */
 
-void	initialize_server(void)
+static void	initialize_server(void)
 {
 	g_client_status.current_pid = 0;
 	g_client_status.signal_status = SIG_FOR_WAITING;
@@ -111,47 +111,52 @@ void	initialize_server(void)
 	ft_putchar_fd('\n', 1);
 }
 
+static void	handle_bit(void)
+{
+	static char						current_char;
+	static volatile sig_atomic_t	handled_bits_count;
+	pid_t							client_pid;
+
+	client_pid = g_client_status.current_pid;
+	if (client_pid == 0)
+	{
+		current_char = 0;
+		handled_bits_count = 0;
+		return ;
+	}
+	if (g_client_status.signal_status == SIG_FOR_WAITING)
+	{
+		return ;
+	}
+	if (g_client_status.signal_status == SIG_FOR_ONE_BIT)
+	{
+		current_char |= 1 << (7 - handled_bits_count);
+	}
+	handled_bits_count++;
+	if (handled_bits_count == 8)
+	{
+		if (current_char != 0x02 && current_char != 0x03)
+			write(1, &current_char, 1);
+		else if (current_char == 0x03)
+		{
+			write(1, "\n", 1);
+			g_client_status.current_pid = 0;
+		}
+		current_char = 0;
+		handled_bits_count = 0;
+	}
+	g_client_status.signal_status = SIG_FOR_WAITING;
+	if (kill(client_pid, SIGUSR2) < 0)
+		exit_with_error(KILL_FAIL);
+}
+
 int	main(void)
 {
-	bool					is_end_of_transmission;
-	char					current_char;
-	volatile sig_atomic_t	handled_bits_count;
-
 	initialize_server();
-	is_end_of_transmission = false;
-	current_char = 0;
-	handled_bits_count = 0;
 	while (1)
 	{
 		usleep(SLEEP_DURATION);
-		if (g_client_status.signal_status != SIG_FOR_WAITING)
-		{
-			if (g_client_status.signal_status == SIG_FOR_ONE_BIT)
-			{
-				current_char |= 1 << (7 - handled_bits_count);
-			}
-			handled_bits_count++;
-			if (handled_bits_count == 8)
-			{
-				if (current_char != 0x02 && current_char != 0x03)
-					write(1, &current_char, 1);
-				else if (current_char == 0x03)
-				{
-					write(1, "\n", 1);
-					is_end_of_transmission = true;
-				}
-				current_char = 0;
-				handled_bits_count = 0;
-			}
-			g_client_status.signal_status = SIG_FOR_WAITING;
-			if (kill(g_client_status.current_pid, SIGUSR2) < 0)
-				exit_with_error(KILL_FAIL);
-			if (is_end_of_transmission)
-			{
-				g_client_status.current_pid = 0;
-				is_end_of_transmission = false;
-			}
-		}
+		handle_bit();
 	}
 	return (0);
 }
