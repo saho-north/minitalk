95202823a2452a0e1cf35520253e69160fdfa349
saho-north <SahoKitahara@my.uopeople.edu>
2023-08-24 15:05:03 +0900

    ok

--- server.c
+++ server.c
@@ -6,51 +6,42 @@
 /*   By: sakitaha <sakitaha@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/15 03:56:21 by sakitaha          #+#    #+#             */
-/*   Updated: 2023/08/24 07:36:25 by sakitaha         ###   ########.fr       */
+/*   Updated: 2023/08/24 15:03:12 by sakitaha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "server.h"
 
-volatile t_signal_data	g_client_status;
+volatile t_signal_info	g_client_info;
 
 static void	signal_action(int sig, siginfo_t *info, void *ucontext)
 {
 	(void)ucontext;
-	if (g_client_status.signal_status != SIG_FOR_WAITING)
-	{
+	if (g_client_info.signal_status != SIG_FOR_WAITING)
 		return ;
-	}
-	if (g_client_status.current_pid == 0)
-	{
-		g_client_status.current_pid = info->si_pid;
-	}
-	else if (info->si_pid != g_client_status.current_pid)
-	{
+	if (g_client_info.current_pid == 0)
+		g_client_info.current_pid = info->si_pid;
+	else if (info->si_pid != g_client_info.current_pid)
 		return ;
-	}
 	if (sig == SIGUSR1)
-	{
-		g_client_status.signal_status = SIG_FOR_ZERO_BIT;
-	}
+		g_client_info.signal_status = SIG_FOR_ZERO_BIT;
 	else if (sig == SIGUSR2)
-	{
-		g_client_status.signal_status = SIG_FOR_ONE_BIT;
-	}
+		g_client_info.signal_status = SIG_FOR_ONE_BIT;
 }
 
-static void	initialize_server(void)
+static void	init_server(void)
 {
-	g_client_status.current_pid = 0;
-	g_client_status.signal_status = SIG_FOR_WAITING;
-	init_sigaction(signal_action);
+	g_client_info.current_pid = 0;
+	g_client_info.signal_status = SIG_FOR_WAITING;
+	ft_putstr_fd("Server PID: ", 1);
 	ft_putnbr_fd(getpid(), 1);
 	ft_putchar_fd('\n', 1);
 }
 
 int	main(void)
 {
-	initialize_server();
+	init_server();
+	init_sigaction(signal_action);
 	while (1)
 	{
 		usleep(SLEEP_DURATION);
