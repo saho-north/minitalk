580cd31fe42fe55822df4691adaf71486a2d2afb
saho-north <SahoKitahara@my.uopeople.edu>
2023-11-17 16:14:50 +0900

    added ack received count

--- srcs/client/transmit_message.c
+++ srcs/client/transmit_message.c
@@ -6,11 +6,20 @@
 /*   By: sakitaha <sakitaha@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/24 22:20:42 by sakitaha          #+#    #+#             */
-/*   Updated: 2023/11/17 15:42:12 by sakitaha         ###   ########.fr       */
+/*   Updated: 2023/11/17 16:07:19 by sakitaha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "client.h"
+
+static void	print_result(size_t ack_count)
+{
+	ft_putstr_fd("\nTotal ACKs received: ", 1);
+	ft_putnbr_fd(ack_count, 1);
+	ft_putstr_fd("\nTotal bytes transmitted: ", 1);
+	ft_putnbr_fd((ack_count - 9) / 8, 1);
+	ft_putchar_fd('\n', 1);
+}
 
 static void	transmit_bit(pid_t pid, char bit)
 {
@@ -26,7 +35,7 @@ static void	transmit_bit(pid_t pid, char bit)
 	}
 }
 
-static void	transmit_byte(pid_t pid, char c)
+static void	transmit_byte(pid_t pid, char c, size_t *ack_count)
 {
 	int				bit_index;
 	size_t			attempt_count;
@@ -44,6 +53,7 @@ static void	transmit_byte(pid_t pid, char c)
 		{
 			bit_index--;
 			attempt_count = 0;
+			(*ack_count)++;
 		}
 		else if (ack_status == ACK_SERVER_FAIL)
 			exit_with_error(SERVER_FAIL);
@@ -52,7 +62,7 @@ static void	transmit_byte(pid_t pid, char c)
 	}
 }
 
-static void	send_initial_signal(pid_t pid)
+static void	send_initial_signal(pid_t pid, size_t *ack_count)
 {
 	size_t			attempt_count;
 	sig_atomic_t	ack_status;
@@ -66,7 +76,10 @@ static void	send_initial_signal(pid_t pid)
 		attempt_count++;
 		ack_status = get_ack_status();
 		if (ack_status == ACK_RECEIVED)
+		{
+			(*ack_count)++;
 			break ;
+		}
 		else if (ack_status == ACK_SERVER_FAIL)
 			exit_with_error(SERVER_FAIL);
 		else if (attempt_count > MAX_ATTEMPTS)
@@ -76,11 +89,15 @@ static void	send_initial_signal(pid_t pid)
 
 void	transmit_message(pid_t pid, const char *str)
 {
-	send_initial_signal(pid);
+	size_t	ack_count;
+
+	ack_count = 0;
+	send_initial_signal(pid, &ack_count);
 	while (*str)
 	{
-		transmit_byte(pid, *str);
+		transmit_byte(pid, *str, &ack_count);
 		str++;
 	}
-	transmit_byte(pid, '\0');
+	transmit_byte(pid, '\0', &ack_count);
+	print_result(ack_count);
 }
